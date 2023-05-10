29b536618c832977495442fe7f1b3e415ec1d345
vauliys <vauliys@gmail.com>
2023-05-10 18:31:47 +1000

    Mobile Nav Appears on Click

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
+    navMenu.classList.toggle('nav-show');
+
 })
+
+
 
 
 /*================ ANIMATION LOAD MORE ================*/
