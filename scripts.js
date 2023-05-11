ea34d6b8d56389ffdbd5996a05040b52425ee9a7
vauliys <vauliys@gmail.com>
2023-05-11 22:09:11 +1000

    New Page: Services (Illustrations)

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
+/*================ SCROLL TO TOP / SCROLL TO ABOUT ================*/
+const scrolltotop = document.getElementById('scroll');
+
+scrolltotop.addEventListener("click", e => {
+    if (window.scrollY > 20) {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    } else {
+        window.scrollTo({
+            top: 750,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+});
+
+/*================ SCROLL TO TOP / ARROW ROTATES ================*/
+
+window.addEventListener('scroll', e => {
+    if (window.scrollY > 20 && !scrolltotop.classList.contains('scroll-up')) {
+        scrolltotop.classList.add('scroll-up');
+    } else if (window.scrollY <= 20 && scrolltotop.classList.contains('scroll-up')) {
+        scrolltotop.classList.remove('scroll-up');
+    }
+});
+
+
+
+
 /*================ NAV ================*/
 
 /* searching for item in html and giving it a Java name*/
@@ -36,10 +68,8 @@ navItems.forEach(item => { /*applying code to EACH item in List*/
 
 
 
-const carousel = document.querySelectorAll('.project-container');
-const carouselNav = document.querySelectorAll('.project-nav')
-
-/*================ SCROLL DOWN - BTN EXPLORE ================*/
+/*================ HOME SCROLL ================*/
+/*================ HOME: BTN EXPLORE ================*/
 
 function explore() {
     window.scrollTo({
@@ -50,105 +80,33 @@ function explore() {
 }
 
 
-/*================ SCROLL TO TOP / SCROLL TO ABOUT ================*/
-const scrolltotop = document.getElementById('scroll');
+/*================ HOME: SERVICE LOAD PRICES ================*/
 
-scrolltotop.addEventListener("click", e => {
-    if (window.scrollY > 20) {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth"
-        });
-    } else {
-        window.scrollTo({
-            top: 750,
-            left: 0,
-            behavior: "smooth"
-        });
-    }
+const priceSketch = document.querySelector('.prices-sketch');
+const priceFlat = document.querySelector('.prices-flat');
+const priceRender = document.querySelector('.prices-render');
+
+const btnSketch = document.querySelector("#btn-sketch");
+const btnFlat = document.querySelector("#btn-flat");
+const btnRender = document.querySelector("#btn-render");
+
+
+
+btnSketch.addEventListener('click', () => {
+    priceSketch.classList.toggle('show');
 });
 
-/*================ SCROLL TO TOP / ARROW ROTATES ================*/
-
-window.addEventListener('scroll', e => {
-    if (window.scrollY > 20 && !scrolltotop.classList.contains('scroll-up')) {
-        scrolltotop.classList.add('scroll-up');
-    } else if (window.scrollY <= 20 && scrolltotop.classList.contains('scroll-up')) {
-        scrolltotop.classList.remove('scroll-up');
-    }
+btnFlat.addEventListener('click', () => {
+    priceFlat.classList.toggle('show'); /*toggles class*/
 });
 
+btnRender.addEventListener('click', () => {
+    priceRender.classList.toggle('show'); /*toggles class*/
+});
 
-
-// /*================ ANIMATION LOAD MORE ================*/
-// function show() {
-//     document.getElementById('show')
-//         .style.display = "block";
-
-//         document.getElementById('btn-load')
-//         .style.display = "none";
-
-//         document.getElementById('btn-hide')
-//         .style.display = "block";
-// }
-
-// function hide() {
-//     document.getElementById('show')
-//         .style.display = "none";
-
-//         document.getElementById('btn-load')
-//         .style.display = "block";
-
-//         document.getElementById('btn-hide')
-//         .style.display = "none";
-// }
+/*================ HOME: SERVICE LOAD PRICES ARROW ROTATES ================*/
 
 
 
 
-// /*================ HOME: SCROLL COLOUR CHANGE ================*/
 
-// window.addEventListener('scroll', e => {
-//     if (window.scrollY > 20 && !scrolltotop.classList.contains('scroll-up')) {
-//         scrolltotop.classList.add('scroll-up');
-//     } else if (window.scrollY <= 20 && scrolltotop.classList.contains('scroll-up')) {
-//         scrolltotop.classList.remove('scroll-up');
-//     }
-// });
-
-// window.addEventListener('scroll', e => {
-//     if (window.scrollY > 100 && !scrolltotop.classList.contains('scroll-up-blue')) {
-//         scrolltotop.classList.add('scroll-up-blue');
-//     } else if (window.scrollY <= 100 && scrolltotop.classList.contains('scroll-up-blue')) {
-//         scrolltotop.classList.remove('scroll-up-blue');
-//     }
-// });
-
-
-// window.addEventListener('scroll', e => {
-//     if (window.scrollY > 700 && !scrolltotop.classList.contains('scroll-up-blue-purple')) {
-//         scrolltotop.classList.add('scroll-up-blue-purple');
-//     } else if (window.scrollY <= 700 && scrolltotop.classList.contains('scroll-up-blue-purple')) {
-//         scrolltotop.classList.remove('scroll-up-blue-purple');
-//     }
-// });
-
-// window.addEventListener('scroll', e => {
-//     if (window.scrollY > 1200 && !scrolltotop.classList.contains('scroll-up-purple')) {
-//         scrolltotop.classList.add('scroll-up-purple');
-//     } else if (window.scrollY <= 1200 && scrolltotop.classList.contains('scroll-up-purple')) {
-//         scrolltotop.classList.remove('scroll-up-purple');
-//     }
-// });
-
-// window.addEventListener('scroll', e => {
-//     if (window.scrollY > 2300 && !scrolltotop.classList.contains('scroll-up-pink')) {
-//         scrolltotop.classList.add('scroll-up-pink');
-//     } else if (window.scrollY <= 2300 && scrolltotop.classList.contains('scroll-up-pink')) {
-//         scrolltotop.classList.remove('scroll-up-pink');
-//     }
-// });
-
-
-/*================ TILL: SCROLL UP CHANGE ================*/
