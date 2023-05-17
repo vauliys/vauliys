e8a33af09859a458ee8c18477a8088271ff46809
vauliys <vauliys@gmail.com>
2023-05-17 21:19:55 +1000

    Service Illustration Dropbox click container instead of button

--- dropbox-scripts.js
+++ dropbox-scripts.js
@@ -8,17 +8,21 @@ const btnBg = document.querySelector("#btn-bg");
 const btnFlat = document.querySelector("#btn-flat");
 const btnRender = document.querySelector("#btn-render");
 
+const dropBox01 = document.querySelector(".dropbox-container01");
+const dropBox02 = document.querySelector(".dropbox-container02");
+const dropBox03= document.querySelector(".dropbox-container03");
+
 const btnChevronUp = document.querySelector(".btn-chevron-up");
 
 /*======*/
-if (btnFlat) {
-    btnFlat.addEventListener('click', () => {
+if (dropBox01) {
+    dropBox01.addEventListener('click', () => {
         priceFlat.classList.toggle('show'); /*toggles class*/
         btnFlat.classList.toggle('btn-chevron-up'); /* toggles arrow rotation */
     });
 }
-if (btnRender) {
-    btnRender.addEventListener('click', e => {
+if (dropBox02) {
+    dropBox02.addEventListener('click', e => {
         priceRender.classList.toggle('show'); /*toggles class*/
         btnRender.classList.toggle('btn-chevron-up');
     });
@@ -26,8 +30,8 @@ if (btnRender) {
 if (null) {
 
 }
-if (btnBg) {
-    btnBg.addEventListener('click', () => {
+if (dropBox03) {
+    dropBox03.addEventListener('click', () => {
         priceBg.classList.toggle('show');
         btnBg.classList.toggle('btn-chevron-up');
     });
