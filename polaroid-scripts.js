fcd8d2f29452ffd577ae12373be43c51baa642f8
vauliys <vauliys@gmail.com>
2023-06-02 22:54:42 +1000

    Dunkso Content Uploaded, tweaked Keyword CSS

--- polaroid-scripts.js
+++ polaroid-scripts.js
@@ -2,12 +2,12 @@
 /*================ Section: Category Slider ================*/
 const catFeatured = document.querySelector('#category-featured');
 const catMotion = document.querySelector('#category-motion');
-const catPrint = document.querySelector('#category-print');
+const catDesign = document.querySelector('#category-design');
 const catGallery = document.querySelector('#category-gallery');
 
 const polFeatured = document.querySelector('.polaroid-featured');
 const polMotion = document.querySelector('.polaroid-motion');
-const polPrint = document.querySelector('.polaroid-print');
+const polDesign = document.querySelector('.polaroid-design');
 const polGallery = document.querySelector('.gallery');
 
 
@@ -15,24 +15,24 @@ const polGallery = document.querySelector('.gallery');
 catMotion.addEventListener('click', () => {
     catMotion.classList.toggle('slider-item-active'); /*toggles class*/
     catFeatured.classList.remove('slider-item-active'); /*removes class*/
-    catPrint.classList.remove('slider-item-active'); /*removes class*/
+    catDesign.classList.remove('slider-item-active'); /*removes class*/
     catGallery.classList.remove('slider-item-active'); /*removes class*/
 
     polMotion.classList.remove('polaroid-hide'); /*toggles class*/
-    polPrint.classList.add('polaroid-hide'); /*toggles class*/
+    polDesign.classList.add('polaroid-hide'); /*toggles class*/
     polFeatured.classList.add('polaroid-hide'); /*toggles class*/
     polGallery.classList.add('polaroid-hide'); /*toggles class*/
 
 
 });
 
-catPrint.addEventListener('click', () => {
-    catPrint.classList.toggle('slider-item-active'); /*toggles class*/
+catDesign.addEventListener('click', () => {
+    catDesign.classList.toggle('slider-item-active'); /*toggles class*/
     catFeatured.classList.remove('slider-item-active'); /*removes class*/
     catMotion.classList.remove('slider-item-active'); /*removes class*/
     catGallery.classList.remove('slider-item-active'); /*removes class*/
 
-    polPrint.classList.remove('polaroid-hide'); /*toggles class*/
+    polDesign.classList.remove('polaroid-hide'); /*toggles class*/
     polMotion.classList.add('polaroid-hide'); /*toggles class*/
     polFeatured.classList.add('polaroid-hide'); /*toggles class*/
     polGallery.classList.add('polaroid-hide'); /*toggles class*/
@@ -42,12 +42,12 @@ catPrint.addEventListener('click', () => {
 catGallery.addEventListener('click', () => {
     catGallery.classList.toggle('slider-item-active'); /*toggles class*/
     catFeatured.classList.remove('slider-item-active'); /*removes class*/
-    catPrint.classList.remove('slider-item-active'); /*removes class*/
+    catDesign.classList.remove('slider-item-active'); /*removes class*/
     catMotion.classList.remove('slider-item-active'); /*removes class*/
 
     polGallery.classList.remove('polaroid-hide'); /*toggles class*/
     polMotion.classList.add('polaroid-hide'); /*toggles class*/
-    polPrint.classList.add('polaroid-hide'); /*toggles class*/
+    polDesign.classList.add('polaroid-hide'); /*toggles class*/
     polFeatured.classList.add('polaroid-hide'); /*toggles class*/
 });
 
@@ -55,12 +55,12 @@ catGallery.addEventListener('click', () => {
 catFeatured.addEventListener('click', () => {
     catFeatured.classList.toggle('slider-item-active'); /*toggles class*/
     catMotion.classList.remove('slider-item-active'); /*removes class*/
-    catPrint.classList.remove('slider-item-active'); /*removes class*/
+    catDesign.classList.remove('slider-item-active'); /*removes class*/
     catGallery.classList.remove('slider-item-active'); /*removes class*/
 
     polFeatured.classList.remove('polaroid-hide'); /*toggles class*/
     polMotion.classList.add('polaroid-hide'); /*toggles class*/
-    polPrint.classList.add('polaroid-hide'); /*toggles class*/
+    polDesign.classList.add('polaroid-hide'); /*toggles class*/
     polGallery.classList.add('polaroid-hide'); /*toggles class*/
 
 });
--- project-scripts.js
+++ project-scripts.js
@@ -1,8 +1,8 @@
 const catChar = document.querySelector('#category-characters');
 const catProps = document.querySelector('#category-props');
 
-const solChar = document.querySelector('.solar-charcters');
-const solProps = document.querySelector('.solar-props');
+const soChar = document.querySelector('.solar-characters');
+const soProps = document.querySelector('.solar-props');
 
 /* when Category item (Character) is clicked perform code*/
 if (catChar) {
@@ -10,8 +10,8 @@ catChar.addEventListener('click', () => {
     catChar.classList.toggle('slider-item-active'); /*toggles class*/
     catProps.classList.remove('slider-item-active'); /*removes class*/
 
-    solChar.classList.remove('polaroid-hide'); /*toggles class*/
-    solProps.classList.add('polaroid-hide'); /*toggles class*/
+    soChar.classList.remove('hide'); /*toggles class*/
+    soProps.classList.add('hide'); /*toggles class*/
 });
 }
 
@@ -21,8 +21,8 @@ catProps.addEventListener('click', () => {
     catProps.classList.toggle('slider-item-active'); /*toggles class*/
     catChar.classList.remove('slider-item-active'); /*removes class*/
 
-    solProps.classList.remove('polaroid-hide'); /*toggles class*/
-    solChar.classList.add('polaroid-hide'); /*toggles class*/
+    soProps.classList.remove('hide'); /*toggles class*/
+    soChar.classList.add('hide'); /*toggles class*/
 });
 }
 
@@ -39,8 +39,8 @@ catXmas.addEventListener('click', () => {
     catXmas.classList.toggle('slider-item-active'); /*toggles class*/
     catEaster.classList.remove('slider-item-active'); /*removes class*/
 
-    cbXmas.classList.remove('polaroid-hide'); /*toggles class*/
-    cbEaster.classList.add('polaroid-hide'); /*toggles class*/
+    cbXmas.classList.remove('hide'); /*toggles class*/
+    cbEaster.classList.add('hide'); /*toggles class*/
 });
 }
 
@@ -50,8 +50,8 @@ catEaster.addEventListener('click', () => {
     catEaster.classList.toggle('slider-item-active'); /*toggles class*/
     catXmas.classList.remove('slider-item-active'); /*removes class*/
 
-    cbEaster.classList.remove('polaroid-hide'); /*toggles class*/
-    cbXmas.classList.add('polaroid-hide'); /*toggles class*/
+    cbEaster.classList.remove('hide'); /*toggles class*/
+    cbXmas.classList.add('hide'); /*toggles class*/
 });
 }
 
