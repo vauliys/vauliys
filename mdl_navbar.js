8a59d65b4d638ff88cc5d0e74fdff11d4a84a72c
Victoria Pritchard <vauliys@gmail.com>
2023-10-24 17:30:31 +1100

    css organisation

--- mdl_navbar.js
+++ mdl_navbar.js
@@ -7,28 +7,26 @@ if (burgerMenu) {
     });
 }
 
-const navHome = document.querySelector(".navbar-dropdown_navbar-home");
+const navLink02 = document.querySelector(".navbar-dropdown-links-02");
+const links02 = document.querySelector(".navbar-links-02");
 
-const navResource = document.querySelector(".navbar-dropdown_navbar-links-02");
-const resourceLinks = document.querySelector(".navbar-links-02");
-
-const navProjects = document.querySelector(".navbar-dropdown_navbar-links-01");
-const projectLinks = document.querySelector(".navbar-links-01");
+const navLink01 = document.querySelector(".navbar-dropdown-links-01");
+const links01 = document.querySelector(".navbar-links-01");
 
 
 const btnResource = document.querySelector("#btn-navbar-links-02");
 const btnProjects = document.querySelector("#btn-navbar-links-01");
 
 
-if (navResource) {
-    navResource.addEventListener('click', () => {
-        resourceLinks.classList.toggle('show'); /* toggles class */
+if (navLink02) {
+    navLink02.addEventListener('click', () => {
+        links02.classList.toggle('show'); /* toggles class */
     });
 }
 
-if (navProjects) {
-    navProjects.addEventListener('click', () => {
-        projectLinks.classList.toggle('show'); /* toggles class */
+if (navLink01) {
+    navLink01.addEventListener('click', () => {
+        links01.classList.toggle('show'); /* toggles class */
     });
 }
 
