54edf87cd5169ac6480527d1fe20329997b3d83f
vauliys <vauliys@gmail.com>
2024-07-07 21:52:31 +1000

    more analytics - collabs.html

---
+++ mdl_metrics.js
@@ -0,0 +1,21 @@
+/* Mobile Metrics */
+const metricslink = document.querySelector("#metrics");
+const metricstab = document.querySelector("#metrics-tab");
+const overlay = document.querySelector("#overlay");
+const arrow = document.querySelector("#arrow");
+
+
+if (metricslink) {
+    metricslink.addEventListener('click', () => {
+        metricstab.classList.toggle('metrics-show'); /* toggles class */
+        overlay.classList.toggle('overlay-show'); /* toggles class */
+    });
+}
+
+const hideMetrics = () => {
+    metricstab.classList.remove('metrics-show'); /* toggles class */
+    overlay.classList.remove('overlay-show'); /* toggles class */
+};
+
+overlay.addEventListener('click', hideMetrics);
+arrow.addEventListener('click', hideMetrics);
