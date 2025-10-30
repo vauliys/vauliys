5a08b062c7584a6999e5e1d50cb1e28487ba61e3
victoria - mac <vauliys@gmail.com>
2025-10-30 15:57:09 +1100

    Clean up

--- 202501_nav.js
+++
@@ -1,9 +0,0 @@
-/* Mobile Nav */
-const menu = document.querySelector(".menu");
-const nav = document.querySelector("nav");
-
-if (menu) {
-    menu.addEventListener('click', () => {
-        nav.classList.toggle('nav-show'); /* toggles class */
-    });
-}
